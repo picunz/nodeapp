fe054e2d49129f7589d517ea013d2869da799362
picunz <kiand64@gmail.com>
2022-09-27 12:05:45 +0200

    aa

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const http = require('http');
 
 const hostname = 'localhost';
-const port = process.env.PORT || port;
+const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200; 
